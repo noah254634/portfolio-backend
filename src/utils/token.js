2b591a136faea2b3dd7367bddd9dbd9d02adfe09
IamKhaemba <noahkhaemba290@gmail.com>
2026-01-30 19:22:37 +0300

    updated the cookie so it becomes cross-site

--- src/utils/token.js
+++ src/utils/token.js
@@ -13,7 +13,7 @@ export const generateToken = (res, user) => {
   res.cookie("token", token, {
     httpOnly: true,        // JS cannot access it
     secure: ENV().node_env, // only sent over HTTPS in production
-    sameSite: "strict",
+    sameSite: "none",    // allow cross-site requests
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
